54e3d016261a3ba2b0108e91d54036b2c7bed92f
Kimoon Kim <kimoon@pepperdata.com>
2018-04-02 14:04:29 -0700

    Add helm

--- tests/setup.sh
+++ tests/setup.sh
@@ -15,6 +15,7 @@ _MY_SCRIPT="${BASH_SOURCE[0]}"
 _MY_DIR=$(cd "$(dirname "$_MY_SCRIPT")" && pwd)
 _KUBERNETES_VERSION=v1.7.5
 _MINIKUBE_VERSION=v0.25.2
+_HELM_VERSION=v2.8.1
 
 _UNAME_OUT=$(uname -s)
 case "${_UNAME_OUT}" in
@@ -29,16 +30,29 @@ export MINIKUBE_WANTREPORTERRORPROMPT=false
 export CHANGE_MINIKUBE_NONE_USER=true
 
 cd $_MY_DIR
-rm -rf bin
-mkdir -p bin
-# Download kubectl, which is a requirement for using minikube.
-curl -Lo bin/kubectl  \
-  https://storage.googleapis.com/kubernetes-release/release/${_KUBERNETES_VERSION}/bin/${_MY_OS}/amd64/kubectl
-chmod +x bin/kubectl
-# Download minikube.
-curl -Lo bin/minikube  \
-  https://storage.googleapis.com/minikube/releases/${_MINIKUBE_VERSION}/minikube-${_MY_OS}-amd64
-chmod +x bin/minikube
+rm -rf tmp
+mkdir -p bin tmp
+if [[ ! -x bin/kubectl ]]
+then
+  # Download kubectl, which is a requirement for using minikube.
+  curl -Lo bin/kubectl  \
+    https://storage.googleapis.com/kubernetes-release/release/${_KUBERNETES_VERSION}/bin/${_MY_OS}/amd64/kubectl
+  chmod +x bin/kubectl
+fi
+if [[ ! -x bin/minikube ]]
+then
+  # Download minikube.
+  curl -Lo bin/minikube  \
+    https://storage.googleapis.com/minikube/releases/${_MINIKUBE_VERSION}/minikube-${_MY_OS}-amd64
+  chmod +x bin/minikube
+fi
+if [[ ! -x bin/helm ]]
+then
+  # Download helm
+  curl -Lo tmp/helm.tar.gz  \
+    https://storage.googleapis.com/kubernetes-helm/helm-${_HELM_VERSION}-${_MY_OS}-amd64.tar.gz
+  (cd tmp; tar xfz helm.tar.gz; mv ${_MY_OS}-amd64/helm ${_MY_DIR}/bin)
+fi
 
 export PATH=${_MY_DIR}/bin:$PATH
 
@@ -62,3 +76,4 @@ until kubectl get nodes -o jsonpath="$_JSONPATH" 2>&1 | grep -q "Ready=True"
 do
   sleep 1
 done
+helm init
---
+++ tests/teardown.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+set -o xtrace
+
+_MY_SCRIPT="${BASH_SOURCE[0]}"
+_MY_DIR=$(cd "$(dirname "$_MY_SCRIPT")" && pwd)
+
+cd $_MY_DIR
+export PATH=${_MY_DIR}/bin:$PATH
+
+minikube status
+minikube stop
+
+rm -rf tmp
+if [[ "${TEARDOWN_DELETE_MINIKUBE:-}" == "true" ]]; then
+  minikube delete
+fi
+if [[ "${TEARDOWN_DELETE_BIN:-}" == "true" ]]; then
+  rm -rf bin
+fi
