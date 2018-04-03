7ade789ae90345f67ba4263dc2986f3c3ecb62c9
Kimoon Kim <kimoon@pepperdata.com>
2018-04-03 13:08:07 -0700

    Clean up

--- tests/lib/_k8s.sh
+++ tests/lib/_k8s.sh
@@ -8,7 +8,7 @@ function _wait_for_ready () {
   shift
   local attempts=40
   echo "Running: $@"
-  while [[ "$count" != $("$@" 2>&1 | tail -n +2 | grep -c "$evidence") ]];
+  while [[ "$count" != $("$@" 2>&1 | tail -n +2 | grep -c $evidence) ]];
   do
     if [[ "$attempts" = 1 ]]; then
       "$@" || true
@@ -31,11 +31,12 @@ function k8s_single_node_ready () {
 }
 
 # Wait for all expected number of pods to be ready. This works only for
-# pods with one container. We check "1/1" in the `kubectl get pods` output.
+# pods with up to 4 containers, as we check "1/1" to "4/4" in
+# `kubectl get pods` output.
 function k8s_all_pods_ready () {
   local count="$1"
   shift
-  _wait_for_ready "$count" "1/1" kubectl get pods "$@"
+  _wait_for_ready "$count" "-e 1/1 -e 2/2 -e 3/3 -e 4/4" kubectl get pods "$@"
 }
 
 function k8s_single_pod_ready () {
