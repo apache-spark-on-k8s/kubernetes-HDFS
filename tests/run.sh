044466d58656e676a266eac9a31bec2a78aee260
Kimoon Kim <kimoon@pepperdata.com>
2018-04-12 17:18:13 -0700

    Support multiple test cases

--- tests/run.sh
+++ tests/run.sh
@@ -22,10 +22,6 @@ source ${_TEST_DIR}/lib/_k8s.sh
 _PROJECT_DIR=$(cd "$(dirname "$_TEST_DIR")" && pwd)
 _CHART_DIR=${_PROJECT_DIR}/charts
 
-cd $_CHART_DIR
-
-kubectl cluster-info
-
 function _run () {
   local attempts=2
   echo Running: "$@"
@@ -35,51 +31,15 @@ function _run () {
   done
 }
 
-_run helm install krb5-server  \
-  --name my-krb5-server
-k8s_single_pod_ready -l app=krb5-server
+kubectl cluster-info
+cd $_CHART_DIR
 
-_run helm install zookeeper  \
-  --name my-zk  \
-  --version 0.6.3 \
-  --repo https://kubernetes-charts-incubator.storage.googleapis.com/  \
-  --set servers=1,heap=100m,resources.requests.memory=100m
-k8s_single_pod_ready -l app=zookeeper
-
-_run helm install hdfs-journalnode-k8s  \
-  --name my-hdfs-journalnode
-k8s_all_pods_ready 3 -l app=hdfs-journalnode
-
-# Disables hostNetwork so namenode pods on a single minikube node can avoid
-# port conflict
-_run helm install hdfs-namenode-k8s  \
-  --name my-hdfs-namenode  \
-  --set hostNetworkEnabled=false,zookeeperQuorum=my-zk-zookeeper-0.my-zk-zookeeper-headless.default.svc.cluster.local:2181
-k8s_all_pods_ready 2 -l app=hdfs-namenode
-
-_run helm install hdfs-datanode-k8s  \
-  --name my-hdfs-datanode  \
-  --set "dataNodeHostPath={/mnt/sda1/hdfs-data}"
-k8s_single_pod_ready -l name=hdfs-datanode
-
-echo All pods:
-kubectl get pods
-
-echo All persistent volumes:
-kubectl get pv
-
-_run helm install hdfs-client  \
-  --name my-hdfs-client
-k8s_single_pod_ready -l app=hdfs-client
-_CLIENT=$(kubectl get pods -l app=hdfs-client -o name| cut -d/ -f 2)
-echo Found client pod $_CLIENT
-
-_run kubectl exec $_CLIENT -- hdfs dfsadmin -report
-_run kubectl exec $_CLIENT -- hdfs haadmin -getServiceState nn0
-_run kubectl exec $_CLIENT -- hdfs haadmin -getServiceState nn1
-
-_run kubectl exec $_CLIENT -- hadoop fs -rm -r -f /tmp
-_run kubectl exec $_CLIENT -- hadoop fs -mkdir /tmp
-_run kubectl exec $_CLIENT -- sh -c  \
-  "(head -c 100M < /dev/urandom > /tmp/random-100M)"
-_run kubectl exec $_CLIENT -- hadoop fs -copyFromLocal /tmp/random-100M /tmp
+_DEFAULT_CASES="*"
+: "${CASES:=$_DEFAULT_CASES}"
+_CASES=$(ls ${_TEST_DIR}/cases/${CASES})
+for _CASE in $_CASES; do
+  echo Running test case: $_CASE
+  source $_CASE
+  run_test_case
+  $_TEST_DIR/cleanup.sh
+done
