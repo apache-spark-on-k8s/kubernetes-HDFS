ed54d63bad801f01842b92fd23cf86b9e4bc5ee3
Kimoon Kim <kimoon@pepperdata.com>
2018-04-13 16:55:32 -0700

    debug travis

--- tests/setup.sh
+++ tests/setup.sh
@@ -121,8 +121,10 @@ echo Showing kube-system pods
 kubectl get -n kube-system pods
 
 _ADDON=$(kubectl get pod -n kube-system -l component=kube-addon-manager --no-headers -o name| cut -d/ -f2)
+echo Addon-manager log:
 kubectl logs -n kube-system $_ADDON
 k8s_all_pods_ready 4 -n kube-system || true
+echo Addon-manager log:
 kubectl logs -n kube-system $_ADDON
 k8s_all_pods_ready 4 -n kube-system
 
